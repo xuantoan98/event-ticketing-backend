1fd7d7773e51e07500012d7f7291820563bd03c8
kevin <kevin@rock.et>
2025-06-20 16:40:55 +0700

    Refactor auth service, controller event feedback

--- src/controllers/Feedback.controller.ts
+++ src/controllers/Feedback.controller.ts
@@ -3,18 +3,14 @@ import { formatResponse } from "../utils/response.util";
 import { IFeedback } from "../interfaces/Feedback.interface";
 import { FeedbackService } from "../services";
 import { AuthMessages, FeedbackMessages } from "../constants/messages";
+import { HTTP } from "../constants/https";
 
 const feedbackService = new FeedbackService();
 
 export const createFeedback = async (req: Request, res: Response) => {
   try {
-    const dataCreate = {
-      ...req.body,
-      createdBy: req.user?._id
-    } as IFeedback;
-
-    const result = await feedbackService.create(dataCreate);
-    return res.status(201).json(
+    const result = await feedbackService.create(req.body, req.user);
+    return res.status(HTTP.CREATED).json(
       formatResponse(
         'success',
         FeedbackMessages.CREATE_SUCCESSFULLY,
@@ -22,7 +18,7 @@ export const createFeedback = async (req: Request, res: Response) => {
       )
     );
   } catch (error) {
-    return res.status(500).json(
+    return res.status(HTTP.INTERNAL_ERROR).json(
       formatResponse(
         'error',
         `Lỗi hệ thống: ${error}`
@@ -33,26 +29,8 @@ export const createFeedback = async (req: Request, res: Response) => {
 
 export const updateFeedback = async (req: Request, res: Response) => {
   try {
-    const user = req.user;
-    const feedback = await feedbackService.getFeedback(req.params.id);
-
-    // Admin có thể sửa phản hồi của user khác và chỉ user đó mới sửa được phản hồi của chính họ
-    if (user?.role !== 'admin' && feedback?.createdBy.toString() !== user?._id.toString()) {
-      return res.status(403).json(
-        formatResponse(
-          'error',
-          AuthMessages.FORBIDDEN
-        )
-      )
-    }
-
-    const dataUpdate = {
-      ...req.body,
-      updatedBy: req.user?._id
-    } as IFeedback;
-
-    const result = await feedbackService.update(req.params.id, dataUpdate);
-    return res.status(200).json(
+    const result = await feedbackService.update(req.params.id, req.body, req.user);
+    return res.status(HTTP.OK).json(
       formatResponse(
         'success',
         FeedbackMessages.UPDATE_SUCCESSFULLY,
@@ -60,7 +38,7 @@ export const updateFeedback = async (req: Request, res: Response) => {
       )
     );
   } catch (error) {
-    return res.status(500).json(
+    return res.status(HTTP.INTERNAL_ERROR).json(
       formatResponse(
         'error',
         `Lỗi hệ thống: ${error}`
@@ -71,23 +49,16 @@ export const updateFeedback = async (req: Request, res: Response) => {
 
 export const deleteFeedback = async (req: Request, res: Response) => {
   try {
-    const user = req.user;
-    const feedback = await feedbackService.getFeedback(req.params.id);
-
-    // Admin có thể xóa phản hồi của user khác và chỉ user đó mới xóa được phản hồi của chính họ
-    if (user?.role !== 'admin' && feedback?.createdBy.toString() !== user?._id.toString()) {
-      return res.status(403).json(
-        formatResponse(
-          'error',
-          AuthMessages.FORBIDDEN
-        )
-      )
-    }
-
     const result = await feedbackService.delete(req.params.id);
-    return result;
+    return res.status(HTTP.OK).json(
+      formatResponse(
+        'success',
+        FeedbackMessages.DELETE_SUCCESSFULLY,
+        result
+      )
+    );
   } catch (error) {
-    return res.status(500).json(
+    return res.status(HTTP.INTERNAL_ERROR).json(
       formatResponse(
         'error',
         `Lỗi hệ thống: ${error}`
--- src/services/Feedback.service.ts
+++ src/services/Feedback.service.ts
@@ -3,36 +3,62 @@ import { EventService } from "./Event.service";
 import Feedback from "../models/Feedback.model";
 import { Types } from "mongoose";
 import { PaginationOptions } from "../interfaces/common/pagination.interface";
+import { IUserDocument } from "../interfaces/User.interface";
+import { ApiError } from "../utils/ApiError";
+import { HTTP } from "../constants/https";
+import { AuthMessages } from "../constants/messages";
+import { Role } from "../constants/enum";
 
 const eventService = new EventService();
 
 export class FeedbackService {
-  async create(dataCreate: IFeedback) {
+  async create(dataCreate: IFeedback, currentUser?: IUserDocument) {
+    if (!currentUser) {
+      throw new ApiError(HTTP.UNAUTHORIZED, AuthMessages.UNAUTHORIZED);
+    }
+
     if (dataCreate.eventId) {
       const eventExit = await eventService.getEventById(dataCreate.eventId.toString());
       if (!eventExit) {
-        throw new Error('Sự kiện không tồn tại trong hệ thống');
+        throw new ApiError(HTTP.NOT_FOUND, 'Sự kiện không tồn tại trong hệ thống');
       }
     }
 
-    const result = await Feedback.create(dataCreate);
+    const dataCreatedBy = {
+      ...dataCreate,
+      createdBy: new Types.ObjectId(currentUser._id),
+    } as IFeedback;
+    const result = await Feedback.create(dataCreatedBy);
+
+    // Sau khi tạo phản hồi, cập nhập lại số lượng phản hồi vào filed totalFeedbacks của sự kiện
+
+
     return result;
   }
 
-  async update(feedbackId: string, dataUpdate: IFeedback) {
+  async update(feedbackId: string, dataUpdate: IFeedback, currentUser?: IUserDocument) {
+    if (!currentUser) {
+      throw new ApiError(HTTP.UNAUTHORIZED, AuthMessages.UNAUTHORIZED);
+    }
+
     if (!Types.ObjectId.isValid(feedbackId)) {
-      throw new Error('ID không hợp lệ');
+      throw new ApiError(HTTP.BAD_REQUEST, 'ID không hợp lệ');
+    }
+
+    // Chỉ user là admin hoặc user tạo phản hồi mới có quyền sửa
+    if (currentUser.role.toString() !== Role.ADMIN.toString() && dataUpdate.createdBy?.toString() !== currentUser._id.toString()) {
+      throw new ApiError(HTTP.FORBIDDEN, 'Bạn không có quyền sửa phản hồi này');
     }
 
     const feedbackExit = await Feedback.findById(feedbackId);
     if (!feedbackExit) {
-      throw new Error('Phản hồi không tồn tại trong hệ thống');
+      throw new ApiError(HTTP.NOT_FOUND, 'Phản hồi không tồn tại trong hệ thống');
     }
 
     if (dataUpdate.eventId) {
       const eventExit = await eventService.getEventById(dataUpdate.eventId.toString());
       if (!eventExit) {
-        throw new Error('Sự kiện không tồn tại trong hệ thống');
+        throw new ApiError(HTTP.NOT_FOUND, 'Sự kiện không tồn tại trong hệ thống');
       }
     }
 
@@ -40,34 +66,51 @@ export class FeedbackService {
     return result;
   }
 
-  async delete(feedbackId: string) {
+  async delete(feedbackId: string, currentUser?: IUserDocument) {
+    if (!currentUser) {
+      throw new ApiError(HTTP.UNAUTHORIZED, AuthMessages.UNAUTHORIZED);
+    }
+
     if (!Types.ObjectId.isValid(feedbackId)) {
-      throw new Error('ID không hợp lệ');
+      throw new ApiError(HTTP.BAD_REQUEST, 'ID không hợp lệ');
+    }
+
+    // Admin có thể xóa phản hồi của user khác và chỉ user đó mới xóa được phản hồi của chính họ
+    if (currentUser?.role.toString() !== Role.ADMIN.toString() && currentUser?._id.toString() !== feedbackId) {
+      throw new ApiError(HTTP.FORBIDDEN, 'Bạn không có quyền xóa phản hồi này');
     }
 
     const feedbackExit = await Feedback.findById(feedbackId);
     if (!feedbackExit) {
-      throw new Error('Phản hồi không tồn tại trong hệ thống');
+      throw new ApiError(HTTP.NOT_FOUND, 'Phản hồi không tồn tại trong hệ thống');
     }
 
     const result = await Feedback.findByIdAndDelete(feedbackId);
     return result;
   }
 
-  async getFeedback(feedbackId: string) {
+  async getFeedback(feedbackId: string, currentUser?: IUserDocument) {
+    if (!currentUser) {
+      throw new ApiError(HTTP.UNAUTHORIZED, AuthMessages.UNAUTHORIZED);
+    }
+
     if (!Types.ObjectId.isValid(feedbackId)) {
-      throw new Error('ID không hợp lệ');
+      throw new ApiError(HTTP.BAD_REQUEST, 'ID không hợp lệ');
     }
 
     const feedbackExit = await Feedback.findById(feedbackId);
     if (!feedbackExit) {
-      throw new Error('Phản hồi không tồn tại trong hệ thống');
+      throw new ApiError(HTTP.NOT_FOUND, 'Phản hồi không tồn tại trong hệ thống');
     }
 
     return await Feedback.findById(feedbackId);
   }
 
-  async getFeedbacks(options: PaginationOptions) {
+  async getFeedbacks(options: PaginationOptions, currentUser?: IUserDocument) {
+    if (!currentUser) {
+      throw new ApiError(HTTP.UNAUTHORIZED, AuthMessages.UNAUTHORIZED);
+    }
+    
     const { page, limit, sortBy, sortOrder } = options;
     const skip = (page - 1) * limit;
     const sortField = sortBy as keyof IFeedback;
